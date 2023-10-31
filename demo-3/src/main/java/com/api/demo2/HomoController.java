ddcf81c9f2e731c8a207a49befa2639d2f38359a
DidulSeneviratne <dulsaradidul@gmail.com>
2023-10-31 16:14:13 +0530

    no message

--- demo-3/src/main/java/com/api/demo2/HomoController.java
+++ demo-3/src/main/java/com/api/demo2/HomoController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +16,31 @@ public class HomoController {
 	@GetMapping("/getall")
 	public List<Employee> getEmployees(){
 		return service.getEmployeeList();
+	}
+	
+	@GetMapping("/getbyid/{id}")
+	public List<Employee> getEmployeeById(@PathVariable("id")int id){
+		return service.findByEmpId(id);
+	}
+	
+	@GetMapping("/getbyname/{name}")
+	public List<Employee> getEmployeeByName(@PathVariable("name")String name){
+		return service.findByEmpName(name);
+	}
+	
+	@GetMapping("/getbydep/{department}")
+	public List<Employee> getEmployeeByDepartment(@PathVariable("department")String department){
+		return service.findByEmpDep(department);
+	}
+	
+	@GetMapping("/getbykeyword/{searchString}")
+	public List<Employee> getEmployeeBySearchString(@PathVariable("searchString")String searchString){
+		return service.findBySearchString(searchString);
+	}
+	
+	@GetMapping("/getbyname_and_dep/{name}/{department}")
+	public List<Employee> getEmployeeByNameAndDep(@PathVariable("name")String name,@PathVariable("department")String department){
+		return service.findByNameAndDepartment(name,department);
 	}
 	
 }
