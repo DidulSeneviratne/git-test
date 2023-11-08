ad64f0573af38d58b6f22a23c2180315e0dccecb
DidulSeneviratne <dulsaradidul@gmail.com>
2023-11-08 09:18:45 +0530

    no message

--- demo-3/src/main/java/com/api/demo2/StaOprs.java
+++ demo-3/src/main/java/com/api/demo2/StaOprs.java
@@ -13,14 +13,22 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class StaOprs {
 	
-	private static final String name = "src/main/resources/data.xlsx";
+	double id;
+	String name;
+	String day;
+	String start;
+	String end;
+	String arrive;
+	String leave;
+	
+	private static final String url = "src/main/resources/data.xlsx";
 	
 	public static List<Station> staList = new ArrayList<Station>();
 	
 	public StaOprs() {
 		
-		/*try {
-			FileInputStream file = new FileInputStream(new File(name));
+		try {
+			FileInputStream file = new FileInputStream(new File(url));
 			XSSFWorkbook workbook = new XSSFWorkbook(file);
 	        XSSFSheet sheet = workbook.getSheetAt(0); // Assuming the first sheet contains the station data
 
@@ -30,41 +38,39 @@ public class StaOprs {
 	            }
 	            
 	            try {
-		            Station station = new Station();
-		            
-		           	station.setStation_id(row.getCell(0).getNumericCellValue());
+		           	id = (row.getCell(0).getNumericCellValue());
 		           	try {
-		           		station.setStation_name(row.getCell(1).getStringCellValue());
+		           		name = (row.getCell(1).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setStation_name("");
+		           		name = "";
 		           	}
 		           	try {
-		           		station.setDay(row.getCell(2).getStringCellValue());
+		           		day = (row.getCell(2).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setDay("");
+		           		day = "";
 		           	}
 		           	try {
-		           		station.setStart(row.getCell(3).getStringCellValue());
+		           		start = (row.getCell(3).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setStart("");
+		           		start = "";
 		           	}
 		           	try {
-		           		station.setEnd(row.getCell(4).getStringCellValue());
+		           		end = (row.getCell(4).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setEnd("");
+		           		end = "";
 		           	}
 		           	try {
-		           		station.setArrive(row.getCell(5).getStringCellValue());
+		           		arrive = (row.getCell(5).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setArrive("");
+		           		arrive = "";
 		           	}
 		           	try {
-		           		station.setLeave(row.getCell(6).getStringCellValue());
+		           		leave = (row.getCell(6).getStringCellValue());
 		           	}catch(Exception e) {
-		           		station.setLeave("");
+		           		leave = "";
 		           	}
-		            	
-		            staList.add(station);
+		           	
+		           	add(id, name, day, start, end, arrive, leave);
 	            
 	            }catch(Exception e) {
 	            	break;
@@ -73,58 +79,20 @@ public class StaOprs {
 	        workbook.close();
 	    } catch (IOException e) {
 	        System.err.println("Error reading Excel file: " + e.getMessage());
-	    }*/
+	    }
 		
-		Station sta1 = new Station();
-		sta1.setStation_id(1);
-		sta1.setStation_name("Angulana");
-		sta1.setDay("Monday");
-		sta1.setStart("Aluthgama");
-		sta1.setEnd("Colombo Fort");
-		sta1.setArrive("04.33AM");
-		sta1.setLeave("04.34AM");
-		staList.add(sta1);
-		
-		Station sta2 = new Station();
-		sta2.setStation_id(2);
-		sta2.setStation_name("Panadura");
-		sta2.setDay("Monday");
-		sta2.setStart("Panadura");
-		sta2.setEnd("Colombo Fort");
-		sta2.setArrive("05.03AM");
-		sta2.setLeave("05.04AM");
-		staList.add(sta2);
-		
-		Station sta3 = new Station();
-		sta3.setStation_id(3);
-		sta3.setStation_name("Mount Lavinia");
-		sta3.setDay("Friday");
-		sta3.setStart("Maradana");
-		sta3.setEnd("Wadduwa");
-		sta3.setArrive("05.05AM");
-		sta3.setLeave("05.06AM");
-		staList.add(sta3);
-		
-		Station sta4 = new Station();
-		sta4.setStation_id(4);
-		sta4.setStation_name("Mount Lavinia");
-		sta4.setDay("Friday");
-		sta4.setStart("Maradana");
-		sta4.setEnd("Wadduwa");
-		sta4.setArrive("05.10AM");
-		sta4.setLeave("05.12AM");
-		staList.add(sta4);
-		
-		Station sta5 = new Station();
-		sta5.setStation_id(5);
-		sta5.setStation_name("Mount Lavinia");
-		sta5.setDay("Monday");
-		sta5.setStart("Maradana");
-		sta5.setEnd("Wadduwa");
-		sta5.setArrive("05.10PM");
-		sta5.setLeave("05.12PM");
-		staList.add(sta5);
-		
+	}
+	
+	public void add(double id, String name, String day, String start, String end, String arrive, String leave){
+		Station sta = new Station();
+		sta.setStation_id(id);
+		sta.setStation_name(name);
+		sta.setDay(day);
+		sta.setStart(start);
+		sta.setEnd(end);
+		sta.setArrive(arrive);
+		sta.setLeave(leave);
+		staList.add(sta);
 	}
 	
 	public List<Station> getStation(){
